a3510885abd5031177740395e23fe58a65a3aebf
brachetto <102389465+marraviglioso@users.noreply.github.com>
2022-05-10 08:02:40 +0200

    V 1.01
    Fixed about page

--- lib/screens/about_page.dart
+++ lib/screens/about_page.dart
@@ -1,117 +1,104 @@
 import 'package:flutter/material.dart';
+import 'package:draggable_home/draggable_home.dart';
 
 class AboutScreen extends StatelessWidget {
   const AboutScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // ignore: unnecessary_new
-    return new Scaffold(
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.deepPurple,
-                image: DecorationImage(
-                  image: AssetImage(
-                    'assets/media/menu.png',
-                  ),
-                  fit: BoxFit.contain,
+    return DraggableHome(
+      alwaysShowLeadingAndAction: true,
+      title: const Text('C R Y P T O N I T E'),
+      stretchMaxHeight: 0.50,
+      headerWidget: headerWidget(context),
+      body: [
+        const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: SizedBox(
+            // color: Colors.white,
+            height: 250,
+            child: Image(image: AssetImage('assets/media/about.png')),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20),
+          child: Column(
+            children: const [
+              Text(
+                'Credits',
+                style: TextStyle(fontSize: 25),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Text('Data provided by CoinGeckoAPI'),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  'For bugs and issues:',
+                  style: TextStyle(fontSize: 25),
                 ),
               ),
-              child: null,
-            ),
-            ListTile(
-              leading: const Icon(
-                Icons.home,
+              Text('Please contact the author: "Marraviglioso" on Github')
+            ],
+          ),
+        ),
+      ],
+      drawer: SafeArea(
+        child: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              const DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.deepPurple,
+                  image: DecorationImage(
+                    image: AssetImage(
+                      'assets/media/menu.png',
+                    ),
+                    fit: BoxFit.contain,
+                  ),
+                ),
+                child: null,
               ),
-              title: const Text('Homepage'),
-              onTap: () {
-                Navigator.pop(context);
-                Navigator.pushReplacementNamed(context, '/home');
-              },
-            ),
-            ListTile(
-              leading: const Icon(
-                Icons.bug_report_rounded,
+              ListTile(
+                leading: const Icon(
+                  Icons.home,
+                ),
+                title: const Text('Homepage'),
+                onTap: () {
+                  Navigator.pop(context);
+                  Navigator.pushReplacementNamed(context, '/home');
+                },
               ),
-              title: const Text('Report bugs'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-          ],
+              ListTile(
+                leading: const Icon(
+                  Icons.bug_report_rounded,
+                ),
+                title: const Text('Report bugs'),
+                onTap: () {
+                  Navigator.pop(context);
+                  Navigator.pushReplacementNamed(context, '/about');
+                },
+              ),
+            ],
+          ),
         ),
       ),
-      backgroundColor: Colors.white,
-      body: CustomScrollView(
-        slivers: <Widget>[
-          const SliverAppBar(
-            title: Text('C R Y P T O N I T E'),
-            backgroundColor: Colors.deepPurple,
-            floating: false,
-            pinned: false,
-            centerTitle: true,
-            expandedHeight: 90,
-            stretch: true,
-            flexibleSpace: FlexibleSpaceBar(),
-          ),
-          SliverToBoxAdapter(
-            child: Container(
-              color: Colors.deepPurple,
-              height: 20,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-                  Container(
-                    height: 20,
-                    decoration: const BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(30.0),
-                        topRight: Radius.circular(30.0),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                color: Colors.white,
-                height: 250,
-                child: const Image(image: AssetImage('assets/media/about.png')),
-              ),
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20),
-              child: Column(
-                children: [
-                  Text(
-                    'About',
-                    style: TextStyle(fontSize: 25),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Text('Data provided by CoinGeckoAPI'),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(10),
-                    child: Text(
-                      'For bugs and issues:',
-                      style: TextStyle(fontSize: 25),
-                    ),
-                  ),
-                  const Text(
-                      'Please contact the author: "Marraviglioso" on Github')
-                ],
+    );
+  }
+
+  Widget headerWidget(BuildContext context) {
+    return Container(
+      color: Colors.deepPurple,
+      child: Row(
+        children: const [
+          Padding(
+            padding: EdgeInsets.only(left: 15.0),
+            child: Align(
+              child: Text(
+                'About',
+                style: TextStyle(color: Colors.white, fontSize: 38),
               ),
             ),
           ),
